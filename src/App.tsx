21aacb3c4c698e5611cb1fd23f0f0b96643fe585
Dario Milani <dariomilani855@gmail.com>
2025-02-03 00:13:34 +0100

    working on adding tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export const App = () => {
-  return <div className="App">TodoList</div>;
+  const [value, setValue] = useState<string>("");
+  const [task, setTask] = useState();
+
+  const Tasks = [
+    { ass: "clean your room", done: true },
+    { ass: "wash the plates", done: false },
+    { ass: "wash the car", done: false },
+  ];
+  return (
+    <div className="App">
+      <div className="title">To Do List:</div>
+      <div className="searchbar">
+        <input
+          placeholder="add task .."
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={() => setTask(task)}>Add</button>
+      </div>
+      {Tasks.map((item, index) => {
+        return (
+          <ul key={index}>
+            <li>{item.ass}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
